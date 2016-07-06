9dc9282beaaf60c01b8abbc0c73c01e6feeb2b04
sqrt <sqrt@iMac-de-sqrt.fritz.box>
2016-07-06 02:58:50 +0200

    remove react router active component after update to react 15.2.0

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-import activeComponent from 'react-router-active-component'
-var NavItem = activeComponent('li');
-
-class App extends React.Component { 
+class App extends React.Component {
   render() {
     return (
       <div className="container">
         <ul>
-          <NavItem to="/" onlyActiveOnIndex>Home</NavItem>
-          <NavItem to="/about">About</NavItem>
+          <li><Link to="/" onlyActiveOnIndex activeClassName="active">Home</Link></li>
+          <li><Link to="/about" activeClassName="active">About</Link></li>
         </ul>
         {this.props.children}
       </div>
-    ); 
+    );
   }
 }
 
 export default App;
+
+// import React from 'react'
+//
+// import activeComponent from 'react-router-active-component'
+// var NavItem = activeComponent('li');
+//
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="container">
+//         <ul>
+//           <NavItem to="/" onlyActiveOnIndex>Home</NavItem>
+//           <NavItem to="/about">About</NavItem>
+//         </ul>
+//         {this.props.children}
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
