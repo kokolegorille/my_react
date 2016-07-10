8d2005d650e13cad0660bb018532c85ac4b3f96c
sqrt <sqrt@iMac-de-sqrt.fritz.box>
2016-07-11 00:33:17 +0200

    Restore react-router-active-component

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router'
+
+import activeComponent from 'react-router-active-component'
+var NavItem = activeComponent('li');
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <ul>
-          <li><Link to="/" onlyActiveOnIndex activeClassName="active">Home</Link></li>
-          <li><Link to="/about" activeClassName="active">About</Link></li>
+          <NavItem to="/" onlyActiveOnIndex>Home</NavItem>
+          <NavItem to="/about">About</NavItem>
         </ul>
         {this.props.children}
       </div>
@@ -16,24 +18,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// import React from 'react'
-//
-// import activeComponent from 'react-router-active-component'
-// var NavItem = activeComponent('li');
-//
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="container">
-//         <ul>
-//           <NavItem to="/" onlyActiveOnIndex>Home</NavItem>
-//           <NavItem to="/about">About</NavItem>
-//         </ul>
-//         {this.props.children}
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
