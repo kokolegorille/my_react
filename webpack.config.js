915cfe1a79d0b39b19c4e26d6a3eb9ce989cfefe
sqrt <sqrt@iMac-de-sqrt.fritz.box>
2017-03-30 19:59:36 +0200

    Fix Update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,6 @@ if(prod) {
       warnings: false
     }
   }));
-  plugins.push(new Webpack.optimize.OccurenceOrderPlugin());
-  plugins.push(new Webpack.optimize.DedupePlugin());
 }
 
 // Production mode will remove code starting with if(__DEV__)
@@ -78,84 +76,3 @@ module.exports = {
   },
   plugins: plugins
 }
-
-// const path = require('path');
-// const ROOT_PATH = path.resolve(__dirname);
-// const SRC_PATH = path.resolve(ROOT_PATH, 'src');
-// const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
-//
-// const Webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-//
-// // Create index.html with correct bundle-[hash] name
-// // Check that output filename start with / for absolute path
-// const plugins = [
-//   new HtmlWebpackPlugin({
-//     filename: 'index.html',
-//     template: './src/index.template',
-//     inject: 'body'
-//   })
-// ];
-//
-// // Check if webpack is run through npm run production
-// const prod = process.argv[4] === '--production';
-//
-// // Optimization
-// // https://github.com/webpack/docs/wiki/optimization
-// if(prod) {
-//   // production
-//   // http://dev.topheman.com/make-your-react-production-minified-version-with-webpack/
-//   plugins.push(new Webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }));
-//
-//   plugins.push(new Webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//       warnings: false
-//     }
-//   }));
-//   plugins.push(new Webpack.optimize.OccurenceOrderPlugin());
-//   plugins.push(new Webpack.optimize.DedupePlugin());
-// }
-//
-// // Production mode will remove code starting with if(__DEV__)
-// // eg. console.log
-// plugins.push(new Webpack.DefinePlugin({
-//   __PRODUCTION__: prod,
-//   __DEV__: !prod
-// }));
-//
-// const config = {
-//   entry: {
-//     bundle: SRC_PATH + '/js/index'
-//   },
-//   resolve: {
-//     extensions: ['', '.js', '.jsx']
-//   },
-//   output: {
-//     path: BUILD_PATH,
-//     publicPath: '',
-//     filename: '/[name]-[hash].js'
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /node_modules/,
-//         loader: "babel",
-//         query: {
-//           presets: ['es2015','react', 'stage-0']
-//         },
-//         include: SRC_PATH
-//       }
-//     ]
-//   },
-//   devServer: {
-//     historyApiFallback: true
-//   },
-//   plugins: plugins
-// };
-//
-// module.exports = config;
