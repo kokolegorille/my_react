3c540bffa61063aadcd8cf8a1a91011d8331a003
sqrt <sqrt@iMac-de-sqrt.fritz.box>
2017-03-30 19:48:55 +0200

    Update to webpack 2

--- src/js/components/root.jsx
+++ src/js/components/root.jsx
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react';
+import { Router, browserHistory } from 'react-router';
 
 import routes from '../routes';
 
 const Root = () => {
+  const history = browserHistory;
+  
   return (
-  <Router>{routes}</Router>
+  <Router history={history}>{routes}</Router>
 );}
 
 export default Root;
